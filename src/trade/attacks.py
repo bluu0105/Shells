51340fb67d80ad03fa3b32461ce2766044a674a8
Matthew Zou <mzou1212@gmail.com>
2024-02-13 21:10:05 -0500

    /attack minor ui updates

--- src/trade/attacks.py
+++ src/trade/attacks.py
@@ -116,8 +116,9 @@ class AttacksCog(commands.Cog):
             
             score_calculation = trade.utils.size_calc(select_1.values[0]) + trade.utils.finish_calc(select_2.values[0]) + trade.utils.color_calc(select_3.values[0]) + trade.utils.shading_calc(select_4.values[0]) + trade.utils.background_calc(select_5.values[0])
             content = f"{interaction.user.mention} has attacked {victim.mention} for {score_calculation} points!"
-            final_embed = discord.Embed(title=f"{interaction.user.name}: {message}", description="", color=discord.Colour.light_embed())
-            final_embed.set_author(name="Art Fight", icon_url=interaction.guild.icon.url)
+            final_embed = discord.Embed(title="", description="", color=discord.Colour.light_embed())
+            final_embed.set_author(name=f"{interaction.user.name}: {message}", icon_url=interaction.user.avatar.url)
+            final_embed.set_footer(text="Art Fight", icon_url=interaction.guild.icon.url)
             final_embed.set_image(url="attachment://image.png")
             image_file = await image.to_file(filename="image.png")
             sent_message = await interaction.channel.send(content=content, embed=final_embed, file=image_file, view=None)
@@ -366,7 +367,7 @@ class AttacksCog(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def deleteattack(self, interaction: discord.Interaction, attack_id: str):
         
-        permitted_users = ["skarpetky", "spectregray", "___bryant"]
+        permitted_users = ["skarpetky", "spectregray", "___bryant", "silentvoice._."]
         curr_attacks = self.db_ref_attacks.get()
         if not (interaction.user.name in permitted_users):
             await interaction.response.send_message("You don't have permission to delete attacks", ephemeral=True)
